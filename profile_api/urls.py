a21dc6d5640f1e310bb180130323ac418047abc1
Douglas Tietjen <douglas@pitch59.com>
2021-11-11 15:45:07 -0700

    User Profile Viewset Added

---
+++ profile_api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from profile_api import views
+
+router = DefaultRouter()
+router.register('basic-viewset', views.BasicViewSet, basename="basic-viewset")
+router.register('profile', views.UserProfileViewSet)
+
+urlpatterns = [
+    path('basic-api-view/', views.BasicApiView.as_view()),
+    path('', include(router.urls))
+]
--- profile_api/views.py
+++ profile_api/views.py
@@ -1,3 +1,94 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, viewsets
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+from profile_api import serializers
+from profile_api import models
+from profile_api import permissions
+
+class BasicApiView(APIView):
+    """Test API View"""
+    serializer_class = serializers.BasicSerializer
+
+    def get(self, request, format=None):
+        """Return a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your application logic',
+            'Is mapped manually to URLs',
+        ]
+
+        return Response({'message': 'Basic API View', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle partial update of an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request):
+        """Delete an object"""
+        return Response({'method': 'DELETE'})
+
+class BasicViewSet(viewsets.ViewSet):
+    """Basic API ViewSet"""
+    serializer_class = serializers.BasicSerializer
+    
+    def list(self, request):
+        """Return a hello message"""
+        a_viewsets = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLS using Routers',
+            'Provides more functionality with less code',
+        ]
+        return Response({'message': 'Basic API Viewset', 'a_viewset': a_viewsets})
+
+    def create(self, request):
+        """Create new hello message"""
+        serializer = self.serializer_class(data.request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+        return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
